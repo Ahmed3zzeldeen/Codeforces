825498374d3c88b92f5c064efeb1bf182e7f2ecd
Ahmed M. Ezzeldeen <Ahmed.Mohamed.Ezzeldeen@gmail.com>
2023-04-09 17:20:56 +0200

    Update : P_320A_RaisingBacteria.java

--- A-set/P_320A_RaisingBacteria.java
+++ A-set/P_320A_RaisingBacteria.java
@@ -1,13 +1,10 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class P_320A_RaisingBacteria {
   public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-    int x = input.nextInt(), count = 0;
-    String s = Integer.toBinaryString(x);
-    for (int i = 0; i < s.length(); i++) {
-      count += (s.charAt(i) == '1') ? 1 : 0;
+    try (Scanner input = new Scanner(System.in)) {
+      int n = input.nextInt();
+      System.out.println(Integer.bitCount(n));
     }
-    System.out.println(count);
   }
 }
